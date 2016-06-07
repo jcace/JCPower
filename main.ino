e4e7a9002e1c2c78deb935474136540d759c848c
jcihelka <jason.cihelka@skf.com>
2016-06-06 19:39:23 -0600

    Add code for web variables

--- main.ino
+++ main.ino
@@ -4,6 +4,8 @@
 
 VoltageMonitor volt;
 CurrentMonitor curr;
+bool _isEnabled;
+unsigned long lastTime = 0UL;
 
 void setup() {
 
@@ -34,23 +36,77 @@ pinMode(boostPin, OUTPUT);
    tone(buzzerPin,1000,400);
    delay (400);
    tone(buzzerPin, 1200, 400);
+
+   // Declare the Spark function for cloud connectivity.
+   Spark.function("webset",WebSet);
+   _isEnabled = true;
+
  }
 
 void loop()
 {
-  digitalWrite(heartbeatLedPin, HIGH);
-  delay (500);
-  digitalWrite(heartbeatLedPin, LOW);
-  delay (500);
-
-
+  unsigned long now = millis();
+    if (now-lastTime/500UL > 1UL)
+    {
+  digitalWrite(heartbeatLedPin, !digitalRead(heartbeatLedPin));
   volt.SetDesiredVoltage(19800);
   curr.SetDesiredCurrent(200);
-  Serial.println("Aa");
-  //Serial.println("voltage:");
-  //Serial.println(volt.ReadSenseVoltage());
   Serial.println("current:");
   Serial.println(curr.ReadSenseCurrent());
+    }
+  //Serial.println("voltage:");
+  //Serial.println(volt.ReadSenseVoltage());
   //Serial.println("vbat:");
   //Serial.println(volt.ReadBatteryVoltage());
+  if (now-lastTime>5000UL) {
+          lastTime = now;
+          // now is in milliseconds
+          char publishString[100];
+          sprintf(publishString,"{\"v\":%d,\"c\":%d}", volt.ReadSenseVoltage(), curr.ReadSenseCurrent());
+          Spark.publish("Stats",publishString);
+      }
+}
+
+int WebSet(String command) {
+  int c, enable, v = 0;
+    // Copy the command string to an array of chars, split based on "-" characters
+    char * params = new char[command.length() + 1];
+    strcpy(params, command.c_str());
+    char * p = strtok(params, "-");
+
+// Step through each of the command settings.
+int commandStep = 0;
+while (p != NULL)
+{
+  if (commandStep = 0)
+  {
+    int enable = atoi(p); // Enable setting, 1 = output, 0 = off.
+    enable = atoi(p); // Enable setting, 1 = output, 0 = off.
+  }
+  if (commandStep = 1)
+  {
+    int voltage = atoi(p);
+    v = atoi(p); // Voltage setting in mV
+  }
+  if (commandStep = 2)
+  {
+    int current = atoi(p);
+    c = atoi(p); // Current setting in mA
+  }
+}
+if (enable != 1)
+  {
+  _isEnabled = false;
+  curr.SetDesiredCurrent(0);
+  volt.SetDesiredVoltage(0);
+  return 0;
+  }
+  else
+  {
+    _isEnabled = true;
+    curr.SetDesiredCurrent(c);
+    volt.SetDesiredVoltage(v); // TODO: Handle the type conversions here
+    return 1;
+  }
+  return 1; // TODO: return 0, return -1 for errors?
 }
